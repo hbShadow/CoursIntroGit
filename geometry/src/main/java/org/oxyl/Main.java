0caa092f04bc96e52bed296461d957c2f6d741ed
hugo <hugo.berthon@epfedu.fr>
2023-02-17 14:39:25 +0100

    feat : add array to main

--- geometry/src/main/java/org/oxyl/Main.java
+++ geometry/src/main/java/org/oxyl/Main.java
@@ -1,11 +1,18 @@
 package org.oxyl;
 
 
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String [] args){
         System.out.println("It works");
         System.out.println("gjdfv");
+        ArrayList<Integer> array = new ArrayList();
+        array.add(1);
+        array.add(1);
+        array.add(1);
+        array.add(1);
+        System.out.println(array);
 
     }
 }
