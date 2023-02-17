3a2d72ecf4f95e6517940bf5537c3edfe8ec1d13
hugo <hugo.berthon@epfedu.fr>
2023-02-17 15:09:27 +0100

    feat : add map

--- geometry/src/main/java/org/oxyl/Main.java
+++ geometry/src/main/java/org/oxyl/Main.java
@@ -2,6 +2,9 @@ package org.oxyl;
 
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String [] args){
@@ -14,15 +17,85 @@ public class Main {
         array.add(1);
         array.add(1);
         System.out.println(array);
+        Map<Integer, String> map = new Map() {
+            @Override
+            public int size() {
+                return 0;
+            }
 
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object key) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object value) {
+                return false;
+            }
+
+            @Override
+            public Object get(Object key) {
+                return null;
+            }
+
+            @Override
+            public Object put(Object key, Object value) {
+                return null;
+            }
+
+            @Override
+            public Object remove(Object key) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map m) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Set keySet() {
+                return null;
+            }
+
+            @Override
+            public Collection values() {
+                return null;
+            }
+
+            @Override
+            public Set<Entry> entrySet() {
+                return null;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return false;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+        };
         System.out.println("gjdfv");
         System.out.println("gjdfv");
-        System.out.println("gjdfv");System.out.println("gjdfv");
         System.out.println("gjdfv");
         System.out.println("gjdfv");
         System.out.println("gjdfv");
         System.out.println("gjdfv");
-        System.out.println("gjdfv");System.out.println("gjdfv");System.out.println("gjdfv");System.out.println("gjdfv");System.out.println("gjdfv");
+        System.out.println("gjdfv");
+        System.out.println("gjdfv");
 
 
 
